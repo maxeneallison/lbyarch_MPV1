d314802662d156c313f9488c66e404a23ae7bcd8
maxeneallison <110168289+maxeneallison@users.noreply.github.com>
2024-07-09 21:24:02 +0800

    Fixed SP

--- asmfunc1.asm
+++ asmfunc1.asm
@@ -2,65 +2,83 @@
 
 section .text
 bits 64
-default rel
+default rel 
 global cvtRGBToGray
 
 cvtRGBToGray:
-    ; Parameters:
-    ; rdx - img1 (source RGB image)
-    ; rcx - img2 (destination grayscale image)
-    ; r8  - width
-    ; r9  - height
+    ; Parameters (volatile):
+        ; rcx - img2 (destination grayscale image)
+        ; rdx - img1 (source RGB image)
+        ; r8  - m (width)
+        ; r9  - n (height)
+    
 
-    push rbp
-    mov rbp, rsp
-    push rbx
-    push r12
-    push r13
-    push r14
+    push rbp                
+    mov rbp, rsp           
+    
+    ;  non-volatile registers
+    push rsi                
+    push rdi                
+    push r12                
+    push r13                
+    push r14    
     push r15
+                
+    ; allocate shadow space and ensure 16-byte alignment
+    sub rsp, 8*5            ; shadow space (5 * 8)
+                            ; This provides the required 32 bytes of shadow space
+                            ; plus 8 additional bytes to ensure 16-byte alignment
 
-    ; Store parameters in non-volatile registers
-    mov r12, rdx    ; Source image pointer
-    mov r13, rcx    ; Destination image pointer
-    mov r14, r8     ; Width
-    mov r15, r9     ; Height
+    ; input parameters in non-volatile registers
+    mov r12, rcx            ; Store img2 pointer (destination)
+    mov r13, rdx            ; Store img1 pointer (source)
+    mov r14, r8             ; Store width in r14
 
-    ; Calculate total number of pixels
-    mov rax, r14
-    mul r15
-    mov rbx, rax    ; Total pixels in rbx
+    ; compute total number of pixels (w*h)
+    mov rax, r8             
+    mul r9                  ; Multiply width by height 
+    mov r15, rax            
 
 pixel_loop:
-    ; Get RGB values
-    movzx r8, byte [r12]      ; Red
-    movzx r9, byte [r12 + 1]  ; Green
-    movzx r10, byte [r12 + 2] ; Blue
+    xor rax, rax            ; for RGB values
 
-    ; Calculate grayscale value (average of R, G, B)
-    mov rax, r8
-    add rax, r9
+    ; average of RGB
+    movzx r10, byte [r13]     ; Red
     add rax, r10
-    xor rdx, rdx
-    mov rcx, 3
-    div rcx         ; rax now contains the average
+    
+    movzx r10, byte [r13 + 1] ; Green
+    add rax, r10
+    
+    movzx r10, byte [r13 + 2] ; Blue
+    add rax, r10
 
-    ; Store grayscale value
-    mov [r13], al
+    ; compute average
+    mov r10, 3
+    xor rdx, rdx            ; Clear upper bits for division
+    div r10
 
-    ; Move to next pixel
-    add r12, 3  ; Move source pointer (RGB = 3 bytes)
-    inc r13     ; Move destination pointer
+    ; store result in the destination image
+    mov [r12], al
 
-    ; Check if we're done
-    dec rbx
-    jnz pixel_loop
+    ; move to next pixel
+    add r13, 3              ; Move source pointer to next pixel (RGB = 3 bytes)
+    inc r12                 ; Move destination pointer to next byte
 
-    ; Restore registers and return
+    ; check if done
+    dec r15                 ; Decrement total pixel count
+    jnz pixel_loop          ; If not zero, continue loop
+
+    
+    add rsp, 8*5            ; Remove shadow space (40 bytes)
+
+    ; Restore non-volatile registers
     pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop rbx
-    pop rbp
+    pop r14                 
+    pop r13                 
+    pop r12                 
+    pop rdi                 
+    pop rsi                 
+    
+    ; Restore the original stack frame
+    pop rbp                
     ret
